90a4a5dee33aa7ff0255a682f4c799f2fbccd973
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-04-04 15:36:40 -0500

    Removed Unnecessary Files + Setup the initial code layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
   return (
-    <div className="App">
-       
+    <div>
+      <h3>Hello World!</h3>
     </div>
   );
 }
